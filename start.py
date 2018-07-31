f370cf5160fa3be72e79d9c710e924b2374f85a7
Kurukadi <Mireund@gmail.com>
2018-07-31 20:30:02 +0300

    upd

--- start.py
+++ start.py
@@ -3,6 +3,8 @@ import time
 from keys import *
 
 session = vk.AuthSession(app_id = bot_app_id, user_login = bot_login, user_password = bot_password, scope = 'messages')
+
 vk_api = vk.API(session, v = '5.50')
+
 def send_message(userID):
 	vk_api.messages.send(user_id= userID, message ='check')
