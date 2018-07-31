8dac1723a2e7d1c0cdd22af1d898e421210b2f38
Kurukadi <Mireund@gmail.com>
2018-07-31 20:28:02 +0300

    upd

--- start.py
+++ start.py
@@ -6,6 +6,4 @@ from keys import *
 session = vk.AuthSession(app_id = bot_app_id, user_login = bot_login, user_password = bot_password, scope = 'messages')
 vk_api = vk.API(session, v = '5.50')
 
-#def send_message(UsrId):
-#	vk.messages.send(user_id="284804387", message="check")
 vk_api.messages.send(user_id=check_user_id, message ='check')
